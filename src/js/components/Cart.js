c01df14586f4a3e787a090ed74b016e474659e35
Edyta2801 <edyta.szarowska@interia.pl>
2019-10-28 20:43:08 +0100

    add chamges in imports

--- src/js/components/Cart.js
+++ src/js/components/Cart.js
@@ -1,6 +1,6 @@
-import {select, settings, templates} from '..settings';
-import {utils} from '..utils';
-import CartProduct from './components/CartProduct.js';
+import {select, settings, templates} from '../settings.js';
+import {utils} from '../utils.js';
+import CartProduct from './CartProduct.js';
 
 class Cart {
   constructor(element) {
