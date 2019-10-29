845abd62775791fc3f17d7a0399512779404fce9
Edyta2801 <edyta.szarowska@interia.pl>
2019-10-29 13:53:47 +0100

    add flatpickr plugin options

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -18,7 +18,45 @@ class DatePicker extends BaseWidget {
     const thisWidget = this;
     thisWidget.minDate = new Date(thisWidget.value);
     thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+
+
+    // flatpickr(element, options);
+
+    flatpickr(thisWidget.dom.input, {
+      defaultDate: thisWidget.minDate,
+      dataFormat: "Y-m-d",
+      maxDate: thisWidget.maxDate,
+      minDate: "today",
+      disable: [
+        function (date) {
+          // return true to disable
+          return (date.getDay() === 1);
+        }
+      ],
+      locale: {
+        firstDayOfWeek: 1 // start week on Monday
+      },
+
+      onChange: function (selectedDates, dateStr) {
+        thisWidget.value = dateStr;
+      },
+    });
   }
+
+
+  parseValue(value) {
+    return isNaN(value);
+  }
+
+  isValid() {
+    return true;
+  }
+
+  renderValue() {
+    //console.log('BaseWidget.renderValue:' BaseWidget.renderValue);
+  }
+
+
 }
 
 export default DatePicker;
